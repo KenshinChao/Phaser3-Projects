2997e4272ccade7dd3f057e23d28acbfca60da48
Kenshin Chao <kechchao@ucsc.edu>
2024-01-31 02:14:19 -0800

    add new font to menu

--- Rocket Patrol Mod/src/scenes/Menu.js
+++ Rocket Patrol Mod/src/scenes/Menu.js
@@ -29,7 +29,7 @@ class Menu extends Phaser.Scene {
             frameRate: 30
         })
         let menuConfig = {
-            fontFamily: 'Courier', 
+            fontFamily: 'Serif', 
             fontSize: '28px',
             backgroundColor: '',
             color: '#8436053',
