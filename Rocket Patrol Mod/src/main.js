a84487511447a0b43303c66ddc903376b28cc262
Kenshin Chao <kechchao@ucsc.edu>
2024-01-31 02:18:54 -0800

    added final name and other requirements

--- Rocket Patrol Mod/src/main.js
+++ Rocket Patrol Mod/src/main.js
@@ -1,3 +1,14 @@
+/*Kenshin Chao
+Rocket Patrol: Bundled Wires
+12 Hours
+//Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5) DONE
+//Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses (5) DONE
+//Display the time remaining (in seconds) on the screen (3) DONE
+//Allow the player to control the Rocket after it's fired (1) DONE
+//Create a new title screen (e.g., new artwork, typography, layout) (3) DONE
+//Implement the 'FIRE' UI text from the original game (1) DONE
+//Implement the speed increase that happens after 30 seconds in the original game (1) DONE
+*/
 let config = {
     type: Phaser.AUTO,
     width: 640,
